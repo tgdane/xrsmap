e027536b0f8e5823e175abe51ea589dd0ae0c551
Thomas Dane <thomasgdane@gmail.com>
2016-10-19 10:14:46 +0100

    update docstrings

--- xrsmap/io.py
+++ xrsmap/io.py
@@ -4,18 +4,47 @@ import numpy as np
 from collections import OrderedDict
 import fabio
 
+from . import utils
+
 
 def load(filename, dtype=np.float64):
     """ Wrapper for fabio.open().
 
     Args:
         filename (str): path to data
-        dtype: data type
+        dtype: data type (e.g., int, np.float32, np.float64).
 
     Returns:
-        data (np.ndarray)
+        data (np.ndarray): loaded data.
     """
     return fabio.open(filename).data.astype(dtype)
+
+
+def flexible_load(data):
+    """
+    Flexible loading of data. For some fields, e.g. dark, flat, background,
+    user can pass a path to file (str), list of paths to be averaged (list)
+    or directly the array (np.ndarray). This function just checks the type
+    and returns np.ndarray.
+
+    Args:
+        data (str, list or np.ndarray): input data of various types.
+
+    Returns:
+        data (np.ndarray):
+    """
+    if data is None:
+        return None
+    try:
+        assert data.__class__ in [str, list, np.ndarray]
+    except AssertionError:
+        raise IOError('Tried to load data: {}, but was not of type:'
+                      'list, string or np.ndarray'.format(data))
+    if data.__class__ is list:
+        data = utils.average_images(data)
+    elif data.__class__ is str:
+        data = io.load(data)
+    return data
 
 
 COMPRESSED_FORMATS = ['bz2', 'gz']
@@ -23,17 +52,17 @@ COMPRESSED_FORMATS = ['bz2', 'gz']
 
 class Writer(object):
     """
+    General file writer class.
 
+    User passes an instance of xrsmap.Mapper(). Header generated from class
+    Attributes (e.g., mesh_shape, roi, binning, etc.).
     """
     def __init__(self, mpr, filename):
-        """
+        """Initialization.
 
         Args:
-            mpr:
-            filename:
-
-        Returns:
-
+            mpr (xrsmap.Mapper): class instance.
+            filename (str or None): path to save data.
         """
         self.mpr = mpr
         self.hdr_dict = None
@@ -90,18 +119,20 @@ class Writer(object):
                 hdr_list.append('{}: {}'.format(k, v))
         return '\n'.join(['{} {}'.format(hdr_key, i) for i in hdr_list])
 
-    def save(self, filename, data, mode, hdr_note=None):
+    def save(self, filename, data, mode, hdr_note=None, extn='edf'):
         """Main save function.
 
         Args:
-            filename:
-            data:
-            mode:
-            hdr_note:
-
-        Returns:
-
+            filename (str): full path to be saved.
+            data (np.ndarray): data.
+            mode (str): e.g. composite, sum_map. Used for selecting header info.
+            hdr_note (str): Additional info the user can pass to be written to
+                the header, e.g., sample info.
+            extn (str): file extension. Currently .edf only supported.
         """
+        # !TODO other data output types
+        if extn != 'edf':
+            raise NotImplementedError('Currently only .edf format supported.')
         if self.hdr_dict is None:
             self.make_header_dict(mode)
 
@@ -118,14 +149,28 @@ class Writer(object):
 
 def get_file_list(dname, fname, numbers=None, check_list=True):
     """
+    Take a directory (dname), file name format (fname) and optionally a range
+    of numbers to yield a list of full paths to the data. File name can
+    contain wild cards (*).
+
+    e.g.
+    ```python
+        get_file_list(dname='/data/expt1/sample1/', fname='d_series_10_*.edf',
+                      numbers=None)
+    ```
+    will look in /data/expt1/sample1/ for all files with format
+    d_series_10_*.edf and return a sorted list of paths.
 
     Args:
-        dname:
-        fname:
-        numbers:
-
+        dname (str): directory
+        fname (str): filename format
+        numbers (str or list): numbers can be passed as list of ints or as a
+            string representing ranges of numbers, e.g., '0-10, 12, 20-26'.
+        check_list (bool): if True will check the generated file list to see
+            if all files exist. This is only really relevant if numbers arg
+            is used. Otherwise files are found with glob.
     Returns:
-
+        file_list (list): list of full paths to all data files.
     """
     if not os.path.isdir(dname):
         raise IOError('Directory does not exist!\n{}'.format(dname))
@@ -172,10 +217,13 @@ def check_file_list(file_list):
     otherwise fail when it gets to it.
 
     Args:
-        file_list:
+        file_list (list): list of paths.
 
     Returns:
+        True: if successful.
 
+    Raises:
+        IOError: if any of the files don't exist.
     """
     for f in file_list:
         if not os.path.exists(f):
@@ -190,10 +238,10 @@ def indices_to_list(indices):
          index_string = '1-3, 5-6, 8-13, 15, 20'
 
     Args:
-        indices:
+        indices (list): list of integers.
 
     Returns:
-
+        index_string (str): string of abbreviated representation of indices.
     """
     index_string = ""
     end = start = indices[0]
@@ -220,10 +268,10 @@ def list_to_indices(index_string):
          indices = [1, 2, 3, 5, 6, 8, 9, 10, 11, 12, 13, 15, 20]
 
     Args:
-        index_string:
+        index_string (str): string of abbreviated representation of indices.
 
     Returns:
-
+        indices (list): list of integers.
     """
     indices = []
     for s in index_string.split(','):
@@ -234,5 +282,3 @@ def list_to_indices(index_string):
         else:
             indices.append(int(s))
     return indices
-
-
